242248fd900a92d182ec038392b484a72c652f9d
dmytro <hryniuk.mitya@gmail.com>
2020-03-03 18:32:07 +0200

    Initial commit

---
+++ app/src/main/java/com/hryniuk/dev/ui/myticket/MyticketFragment.java
@@ -0,0 +1,28 @@
+package com.hryniuk.dev.ui.myticket;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.hryniuk.dev.R;
+
+public class MyticketFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_myticket, container, false);
+
+        return root;
+    }
+}
---
+++ app/src/main/java/com/hryniuk/dev/ui/setting/SettingViewModel.java
@@ -0,0 +1,7 @@
+package com.hryniuk.dev.ui.setting;
+
+import androidx.lifecycle.ViewModel;
+
+public class SettingViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
